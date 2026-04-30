c715e17cbf755f3f10bb2d2dfb1df9bec5292d95
fariaphilia <fzaman675@gmail.com>
2026-04-29 21:37:46 -0400

    webcam.tsx comments

--- photobooth-app/src/components/Webcam.tsx
+++ photobooth-app/src/components/Webcam.tsx
@@ -1,21 +1,22 @@
 import ReactWebcam from "react-webcam"
 import styled from "styled-components";
 
-
-
+// the resolutions for the webcam and the settings passed into
 const videoConstraints = {
     width: 1280,
     height: 720,
+    // this is the front-facing camera
     facingMode: "user"
 };
 
+// centering the webcam and button in one column
 const StyledDiv = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    
 `
 
+// styled button for capturing photo
  const StyledButton = styled.button`
     margin: 20px;
     padding: 10px 30px;
@@ -32,20 +33,24 @@ const StyledDiv = styled.div`
     }
 `
 
-
+// webcam component that accepts setCapturedImage from App.tsx to send the captured photo up to the parent
 const Webcam = ({setCapturedImage} : any) => (
 
     <StyledDiv>
+        {/*using react-webcam which renders a live camera feed*; all other restrictions
+        and variables are given by the npm react-webcam page*/}
 
         <ReactWebcam
             audio={false}
+            // mirrored like a traditional photobooth camera
             mirrored
             height={360}
+            // what happens when you take a picture; it takes a screenshot and saves it as such
             screenshotFormat="image/jpeg"
             width={640}
             videoConstraints={videoConstraints}
+            // styling for the webcam
             style={{
-
                 objectFit: "cover",
                 border: "10px ridge #B562BAFF",
                 borderRadius: "8px",
@@ -55,6 +60,8 @@ const Webcam = ({setCapturedImage} : any) => (
             {/* @ts-expect-error */}
             {({ getScreenshot }) => (
                 <>
+                    {/* Captures a screenshot from the webcam and
+                        sends it up to App.tsx via setCapturedImage */}
                     <StyledButton
                         onClick={() => {
                             const imageSrc = getScreenshot()
@@ -63,9 +70,7 @@ const Webcam = ({setCapturedImage} : any) => (
                     >
                         Capture photo
                     </StyledButton>
-                    {/*<StyledButton onClick={() => setCapturedImage(null)}>*/}
-                    {/*    Retake*/}
-                    {/*</StyledButton>*/}
+
                 </>
             )}
         </ReactWebcam>
